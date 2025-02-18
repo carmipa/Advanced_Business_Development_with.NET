2743c701d3e488bb0e938aff531c559f9337e0cf
carmipa <pabcarminati@gmail.com>
2025-02-17 22:40:25 -0300

    aula2

---
+++ solucao/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ConsoleApp1.Classes;
+
+namespace ConsoleApp1
+{
+    internal class Program
+
+    {   // digite a barra 3x para criar um bloco de comentário
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pessoa _pessoa = new Pessoa();
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public int calcular()
+        {
+            return 1 + 1;
+        }
+    }
+}
