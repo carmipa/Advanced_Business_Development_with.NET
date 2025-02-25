a3463cfeb33b2838edc7e8bf1351ce4f53d3e261
carmipa <pabcarminati@gmail.com>
2025-02-24 22:59:05 -0300

    aula3

---
+++ aulas/aula2/ProjetoNotaAluno/ProjetoNotaAluno/Classes/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNotaAluno.Classes
+{
+    /// <summary>
+    /// Classe que representa um aluno
+    /// </summary>
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int  Idade { get; set; }
+        public List<double> Notas { get; set; }
+
+        /// <summary>
+        /// Construtor padrão
+        /// </summary>
+        public Aluno()
+        {
+        }
+
+        /// <summary>
+        /// Construtor que recebe o nome e a idade do aluno
+        /// </summary>
+        /// <param name="nome"> Nome do aluno</param>
+        /// <param name="idade"> Idade do aluno</param>
+        public Aluno(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            Notas = new List<double>();
+        }
+
+        /// <summary>
+        /// Método que retorna as informações do aluno
+        /// </summary>
+        /// <returns>retorna uma mensagem com o nome e a idade do aluno</returns>
+        public string ObterInformaçoes()
+        {
+            return $"Nome: {Nome}, Idade: {Idade} anos";
+        }
+
+
+
+    }
+}
---
+++ aulas/aula2/ProjetoNotaAluno/ProjetoNotaAluno/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ProjetoNotaAluno.Classes;
+
+namespace ProjetoNotaAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aluno Aluno = new Aluno();
+
+            Console.WriteLine("=== Sistema de cadastro de alunos e notas ===");
+            Console.WriteLine();
+
+            bool continuar = true;
+
+            // Enquanto a variável continuar for true, o programa continuará rodando
+            // enquanto for false, o programa encerrará
+            while (continuar)
+            {
+
+                Console.Write("Digite o nome do aluno: ");
+                string nome = Console.ReadLine();
+
+                
+                
+                Console.Write("Digite a idade do aluno: ");
+                // int idade = int.Parse(Console.ReadLine());
+                
+                if (!int.TryParse(Console.ReadLine(),out int idade))
+                {
+                    Console.WriteLine("Idade inválida! Tente novamente");
+                }
+                
+                Aluno aluno = new Aluno(nome, idade);
+
+                Console.WriteLine(aluno.ObterInformaçoes());
+                Console.WriteLine();
+                
+                //0 > nota1
+                //1 > nota2
+                // 2 > nota3
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+                
+            }
+
+
+        }
+    }
+}
